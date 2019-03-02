2da1e5057435cacdf84b9ce531f55f7883d6dbed
Fisjkars <maxime.escourbiac@gmail.com>
2019-03-02 14:29:54 +0100

    Sh0ts list.

--- app/static/js/common-ajax.js
+++ app/static/js/common-ajax.js
@@ -136,6 +136,22 @@ function ajaxCreateSh0t(success_function, error_function, severity, title, body,
 }
 
 /**
+ * Delete a sh0t.
+ * 
+ * @param {*} success_function function to call in case of ajax success.
+ * @param {*} error_function function to call in case of ajax failure.
+ * @param {*} id Sh0t id.
+ */
+function ajaxDeleteSh0t(success_function, error_function, id){
+    $.ajax({
+        url: "/api/sh0t/" + id,
+        type: 'DELETE',
+        success: success_function,
+        error: error_function
+    });
+}
+
+/**
  * Upload a screenshot for a sh0t.
  * 
  * @param {*} success_function function to call in case of ajax success.
