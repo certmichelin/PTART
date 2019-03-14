f0afc30ac8d7941e81cba9e1f188ad09d751c75c
Maxime Escourbiac <fx13982@pc98.home>
2019-03-14 15:35:48 +0100

    Integrate Markdown for sh0ts body

---
+++ app/static/js/markdown.js
@@ -0,0 +1,8 @@
+
+function activeMarkdown(id){
+    new SimpleMDE({
+        hideIcons: ["side-by-side", "fullscreen"],
+        showIcons: ["code", "table"],
+        forceSync: true,
+        element: $("#" + id)[0] });
+}
