4cb14b730a476f149b517125dddae45e28e844a2
fx13982 <maxime.escourbiac_ext@michelin.com>
2019-03-18 09:31:55 +0100

    Fix bug in API : Authorize blank value

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ from django.db import models
 class Project(models.Model):
 
     name = models.CharField(max_length=100)
-    scope = models.TextField(default="")
+    scope = models.TextField(blank=True, default="")
     added = models.DateTimeField(default=datetime.now)
 
     def __str__(self):  
@@ -98,8 +98,8 @@ class Assessment(models.Model):
 class Sh0t(models.Model):
 
     title = models.CharField(max_length=200)
-    body = models.TextField(default="")
-    asset = models.CharField(max_length=256, default="")
+    body = models.TextField(blank=True, default="")
+    asset = models.CharField(blank=True, max_length=256, default="")
     assessment = models.ForeignKey(Assessment, null=True, on_delete=models.CASCADE)
     added = models.DateTimeField(default=datetime.now)
     severity = models.IntegerField(default=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
@@ -149,8 +149,8 @@ class Screenshot(models.Model):
 class Flag(models.Model):
 
     title = models.CharField(max_length=100)
-    note = models.TextField(default="")
-    asset = models.CharField(max_length=256, default="")
+    note = models.TextField(blank=True, default="")
+    asset = models.CharField(blank=True, max_length=256, default="")
     assessment = models.ForeignKey(Assessment, null=True, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
     added = models.DateTimeField(default=datetime.now)
@@ -167,8 +167,8 @@ class Flag(models.Model):
 class Template(models.Model):
     name = models.CharField(max_length=100)
     severity = models.IntegerField(default=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    body = models.TextField(default="")
-    asset = models.CharField(max_length=256, default="")
+    body = models.TextField(blank=True, default="")
+    asset = models.CharField(blank=True, max_length=256, default="")
 
     def __str__(self):  
         return self.name
@@ -180,7 +180,7 @@ class Template(models.Model):
 """Methodology model."""
 class Methodology(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(default="")
+    description = models.TextField(blank=True, default="")
 
     def __str__(self):  
         return self.name
@@ -192,7 +192,7 @@ class Methodology(models.Model):
 """Module model."""
 class Module(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(default="")
+    description = models.TextField(blank=True, default="")
     methodology = models.ForeignKey(Methodology, on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):  
@@ -206,7 +206,7 @@ class Module(models.Model):
 class Case(models.Model):
     name = models.CharField(max_length=100)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    description = models.TextField(default="")
+    description = models.TextField(blank=True, default="")
 
     def __str__(self):  
         return self.name
