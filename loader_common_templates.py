ab3b079269760b0c97711a86bd4a845200adaa95
Fisjkars <maxime.escourbiac@gmail.com>
2019-05-24 17:00:14 +0200

    Common templates loader.

---
+++ loader_common_templates.py
@@ -0,0 +1,19 @@
+import json
+import os
+import django
+
+#
+# Common templates loader.
+#
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ptart.settings")
+django.setup()
+
+from ptart.models import Template
+
+common_templates_file = open('data/common_templates.json', 'r')
+template_json = json.load(common_templates_file)
+for template in template_json['templates']:
+    new_template = Template(name=template["name"], severity=template["severity"], body=template["body"])
+    new_template.save()
+print("Common templates has been imported !")
