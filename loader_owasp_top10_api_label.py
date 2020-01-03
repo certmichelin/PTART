20a785945d0f6817780d0b666e29ee9e643ff16e
Fisjkars <maxime.escourbiac@gmail.com>
2020-01-03 10:50:56 +0100

    OWASP Top 10 API labels

---
+++ loader_owasp_top10_api_label.py
@@ -0,0 +1,19 @@
+import json
+import os
+import django
+
+#
+# OWASP API Top 10 labels loader.
+#
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ptart.settings")
+django.setup()
+
+from ptart.models import Label
+
+owasp_file = open('data/owasp_top10_api_label.json', 'r')
+label_json = json.load(owasp_file)
+for label in label_json['labels']:
+    new_label = Label(title=label["title"], color=label["color"])
+    new_label.save()
+print("OWASP 2019 API Top 10 labels has been imported !")
