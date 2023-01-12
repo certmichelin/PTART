b24a0a01c547bf23f46bfddfd2cdd07dd5585bce
Fisjkars <maxime.escourbiac@gmail.com>
2023-01-12 15:01:06 +0100

    Quick fix on loader_common_templates.py

--- loader_common_templates.py
+++ loader_common_templates.py
@@ -14,6 +14,6 @@ from ptart.models import Template
 common_templates_file = open('data/common_templates.json', 'r')
 template_json = json.load(common_templates_file)
 for template in template_json['templates']:
-    new_template = Template(name=template["name"], severity=template["severity"], body=template["body"])
+    new_template = Template(name=template["name"], severity=template["severity"], body=template["body"], remediation=template["remediation"])
     new_template.save()
 print("Common templates has been imported !")
