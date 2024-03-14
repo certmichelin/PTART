3d8921c378ebcf909d3b1670c460a992f47b1ac6
Fisjkars <maxime.escourbiac@gmail.com>
2024-03-14 13:55:37 +0100

    adapt tables for cvss

--- ptart/tables.py
+++ ptart/tables.py
@@ -42,7 +42,7 @@ class HitTable(tables.Table):
     assessment = tables.TemplateColumn('<a href="/assessment/{{ record.assessment.pk}}">{{ record.assessment }}</a>')
     fix_complexity = tables.TemplateColumn('<span class="fix-complexity-badge fix-complexity-{{ record.fix_complexity }}">{{ record.get_fix_complexity_str }}</span>')
     displayable = tables.TemplateColumn('{% if record.displayable == True %} <span class="badge badge-success">Displayed</span> {% else %} <span class="badge badge-danger">Hidden</span> {% endif %}', verbose_name= 'Displayable')
-    cvss = tables.TemplateColumn('<span class="cvss cvss-badge cvss-badge-secondary">{{ record.get_cvss_value }}</span>', verbose_name= 'CVSS v3')
+    cvss = tables.TemplateColumn('<span class="cvss cvss-badge cvss-badge-secondary">{{ record.get_cvss_value }}</span>', verbose_name= 'CVSS')
 
     class Meta:
         model = Hit
