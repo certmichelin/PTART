9d13b008af17eadbd2947177f5307da3a0c8012b
Maxime Escourbiac <maxime.escourbiac@michelin.com>
2025-01-17 08:51:38 +0100

    Move loader inside migration script

--- ptart/migrations/0039_cwes_cwe.py
+++ ptart/migrations/0039_cwes_cwe.py
@@ -2,7 +2,32 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
+import xml.etree.ElementTree as ET
 
+""" Insert CWEs and CWEs from XML file """
+def create_cwe_from_xml(apps, schema_editor):
+    CWE = apps.get_model('ptart', 'CWE')
+    CWEs = apps.get_model('ptart', 'CWEs')
+    cwes = CWEs(version="v4.16")
+    cwes.save()
+    
+    tree = ET.parse('data/cwec_v4.16.xml')
+    weaknesses = tree.getroot().find("{http://cwe.mitre.org/cwe-7}Weaknesses")
+    for weakness in weaknesses:
+        
+        extended_description = ""
+        if weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description") is not None:
+            extended_description = weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description").text
+            if extended_description is None:
+                extended_description = weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description").find("{http://www.w3.org/1999/xhtml}p").text
+
+        cwe = CWE(cwes=cwes, 
+                  cwe_id=weakness.attrib['ID'], 
+                  name=weakness.attrib['Name'], 
+                  description=weakness.find("{http://cwe.mitre.org/cwe-7}Description").text, 
+                  extended_description=extended_description)
+        cwe.save()
+    print(": {} CWEs imported !".format(len(weaknesses)))
 
 class Migration(migrations.Migration):
 
@@ -36,4 +61,5 @@ class Migration(migrations.Migration):
                 'ordering': ('pk',),
             },
         ),
+        migrations.RunPython(create_cwe_from_xml),
     ]
