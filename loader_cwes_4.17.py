0ce1f73793f4ed30f0c8468d55af05820582e61a
Maxime Escourbiac <maxime.escourbiac@michelin.com>
2025-06-17 14:50:10 +0200

    Add CWE v4.17 loader script to import weaknesses from XML
    - Created a new script `loader_cwes_4.17.py` for loading CWE v4.17 data.
    - Set up Django environment and imported necessary models.
    - Parsed the CWE XML file and extracted weaknesses.
    - Saved each weakness into the database with relevant fields including ID, name, description, and extended description.
    - Printed the total number of CWEs imported after execution.

---
+++ loader_cwes_4.17.py
@@ -0,0 +1,34 @@
+import json
+import os
+import django
+import xml.etree.ElementTree as ET
+
+#
+# CWE v4.17 loader.
+#
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ptart.settings")
+django.setup()
+
+from ptart.models import Project, CWE, CWEs
+
+cwes = CWEs(version="v4.17")
+cwes.save()
+
+tree = ET.parse('data/cwec_v4.17.xml')
+weaknesses = tree.getroot().find("{http://cwe.mitre.org/cwe-7}Weaknesses")
+for weakness in weaknesses:
+    
+    extended_description = ""
+    if weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description") is not None:
+        extended_description = weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description").text
+        if extended_description is None:
+            extended_description = weakness.find("{http://cwe.mitre.org/cwe-7}Extended_Description").find("{http://www.w3.org/1999/xhtml}p").text
+
+    cwe = CWE(cwes=cwes, 
+                cwe_id=weakness.attrib['ID'], 
+                name=weakness.attrib['Name'], 
+                description=weakness.find("{http://cwe.mitre.org/cwe-7}Description").text, 
+                extended_description=extended_description)
+    cwe.save()
+print("CWE v4.17 : {} CWEs imported !".format(len(weaknesses)))
